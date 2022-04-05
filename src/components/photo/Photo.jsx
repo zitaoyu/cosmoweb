ce38bcc37cb292e636ac1780710b77aa9d4cc273
[zitaoyu] <[zitaoyu.work@gmail.com]>
2022-04-04 22:07:56 -0700

    deploy

--- src/components/photo/Photo.jsx
+++ src/components/photo/Photo.jsx
@@ -2,7 +2,7 @@ import "./photo.css";
 
 const Photo = ({link, img, caption}) => {
   return (
-    <a href={link} target="_blank" rel="noreferrer">
+    <a href={img} target="_blank" rel="noreferrer">
         <img src={img} alt="" className="p-img" />
     </a>
   );
